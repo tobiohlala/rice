fdae1b5855114256552c334b551addd361920d3e
off-world <off_world@gmx.de>
2018-09-17 01:01:46 +0200

    updated tint2 scripts

--- cat-cat/tint2/brightness.sh
+++ cat-cat/tint2/brightness.sh
@@ -2,23 +2,43 @@
 
 cur=$(xbacklight -get | cut -d. -f1)
 
-if (( 0<=cur && cur<=20 )); then
+if (( 0<=cur && cur<=10 )); then
+
+    brightness="│\n│\n│\n│\n╽"
+
+elif (( 11<=cur && cur<=20 )); then
 
     brightness="│\n│\n│\n│\n┃"
 
-elif (( 21<=cur && cur<=40 )); then
+elif (( 21<=cur && cur<=30 )); then
+
+    brightness="│\n│\n│\n╽\n┃"
+
+elif (( 31<=cur && cur<=40 )); then
 
     brightness="│\n│\n│\n┃\n┃"
 
-elif (( 41<=cur && cur<=60 )); then
+elif (( 41<=cur && cur<=50 )); then
+
+    brightness="│\n│\n╽\n┃\n┃"
+
+elif (( 51<=cur && cur<=60 )); then
 
     brightness="│\n│\n┃\n┃\n┃"
 
-elif (( 61<=cur && cur<=80 )); then
+elif (( 61<=cur && cur<=70 )); then
+
+    brightness="│\n╽\n┃\n┃\n┃"
+
+elif (( 71<=cur && cur<=80 )); then
 
     brightness="│\n┃\n┃\n┃\n┃"
 
-elif (( 81<=cur && cur<=100 )); then
+elif (( 81<=cur && cur<=90 )); then
+
+    brightness="╽\n┃\n┃\n┃\n┃"
+
+elif (( 91<=cur && cur<=100 )); then
 
     brightness="┃\n┃\n┃\n┃\n┃"
 
--- cat-cat/tint2/volume.sh
+++ cat-cat/tint2/volume.sh
@@ -3,27 +3,48 @@
 muted=$(amixer get Master | grep "\[" | cut -d[ -f4 | grep -o "[a-z]*")
 cur=$(amixer get Master | grep "\[" | cut -d[ -f2 | grep -o "[0-9]*")
 
-if (( 0==cur )); then
+
+if (( cur<=0 )); then
 
     vol="│\n│\n│\n│\nX"
 
-elif (( 0<cur && cur<=20 )); then
+elif (( 1<=cur && cur<=10 )); then
+
+    vol="│\n│\n│\n│\n╽"
+
+elif (( 11<=cur && cur<=20 )); then
 
     vol="│\n│\n│\n│\n┃"
 
-elif (( 21<=cur && cur<=40 )); then
+elif (( 21<=cur && cur<=30 )); then
+
+    vol="│\n│\n│\n╽\n┃"
+
+elif (( 31<=cur && cur<=40 )); then
 
     vol="│\n│\n│\n┃\n┃"
 
-elif (( 41<=cur && cur<=60 )); then
+elif (( 41<=cur && cur<=50 )); then
+
+    vol="│\n│\n╽\n┃\n┃"
+
+elif (( 51<=cur && cur<=60 )); then
 
     vol="│\n│\n┃\n┃\n┃"
 
-elif (( 61<=cur && cur<=80 )); then
+elif (( 61<=cur && cur<=70 )); then
+
+    vol="│\n╽\n┃\n┃\n┃"
+
+elif (( 71<=cur && cur<=80 )); then
 
     vol="│\n┃\n┃\n┃\n┃"
 
-elif (( 81<=cur && cur<=100 )); then
+elif (( 81<=cur && cur<=90 )); then
+
+    vol="╽\n┃\n┃\n┃\n┃"
+
+elif (( 91<=cur && cur<=100 )); then
 
     vol="┃\n┃\n┃\n┃\n┃"
 
